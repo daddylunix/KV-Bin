440200516c5ef3c5e1f100675307fb5b318c716d
Lunix <ramezjoseph8@gmail.com>
2021-07-17 08:45:54 +0200

    👷 Paste Creation in Dashboard

--- client/src/Components/Create.js
+++ client/src/Components/Create.js
@@ -1,18 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import { useHistory } from "react-router-dom";
 
-const DashboardPage = () => {
-    const [ response, setResponse ] = useState("");
-    const [ pasteResponse, setPasteResponse ] = useState("")
-    const [ title, setTitle ] = useState("");
-    const [ body, setBody ] = useState("");
+const Create = () => {
     const userCookie = Cookies.get('token');
     const history = useHistory();
     if (!userCookie) {
         history.push('/login');
     }
+    const [ response, setResponse ] = useState("");
+    const [ title, setTitle ] = useState("");
+    const [ body, setBody ] = useState("");
     const config = {
         header: {
             "Content-Type":" application/json",
@@ -20,33 +19,20 @@ const DashboardPage = () => {
         },
         withCredentials: true
     }
-    const getInfo = async () => {
-        const data = await axios.get('http://localhost:5000/dashboard', config);
-        setResponse(data.data)
-    }
     const createPaste = async (e) => {
-        e.preventDefault();
         const data = await axios.post('http://localhost:5000/create', {title, body}, config);
-        setPasteResponse(data);
+        console.log(data);
+        e.preventDefault();
     }
-    useEffect(() => {
-     getInfo()   
-    }, [])
     return (
         <div>
-            <h2>Dashboard</h2>
-            <h1>Hello {response.username}</h1>
-            <h2>{JSON.stringify(response)}</h2>
-            <br/>
-            <h2>Create Paste:</h2>
             <form onSubmit={createPaste}>
             <input type="text" value={title} placeholder="title" onChange={(e) => setTitle(e.target.value)}/>
             <input type="text" value={body} placeholder="body" onChange={(e) => setBody(e.target.value)}/>
             <button type="submit">Submit</button>
             </form>
-            {JSON.stringify(pasteResponse.data)}
         </div>
     )
 }
 
-export default DashboardPage
+export default Create
